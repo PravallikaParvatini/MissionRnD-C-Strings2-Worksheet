42bae0928cc72950c74058e02bc609759fc6d366
PravallikaParvatini <PravallikaParvatini@missionrnd.com>
2016-03-22 17:30:19 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -21,6 +21,20 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 
 
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
-
+	int i;
+	*consonants = 0;
+	*vowels = 0;
+	if (str != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if ((str[i] >= 'a'&&str[i] <= 'z') || (str[i] >= 'A'&&str[i] <= 'Z'))
+			{
+				if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+					*vowels = *vowels + 1;
+				else
+					*consonants = *consonants + 1;
+			}
+		}
+	}
 }
--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,6 +11,43 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 
 char * get_last_word(char * str){
-
-	return NULL;
+	int i = 0,len=0,j=0;
+	char *lastWord,temp;
+	lastWord = (char *)malloc(0 * sizeof(char));
+	if (str == NULL)
+		return NULL;
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	while (str[len-1] == ' ')
+		len--;
+	if (len == 0)
+	{
+		lastWord = (char *)realloc(lastWord,1*sizeof(char));
+		lastWord[0] = '\0';
+	}
+	else
+	{
+		while ((len-1) >= 0 && str[len-1] != ' ')
+		{
+			lastWord = (char *)realloc(lastWord, (j + 1)*sizeof(char));
+			lastWord[j] = str[len-1];
+			len--;
+			j++;
+		}
+		lastWord[j] = '\0';
+		i = 0;
+		j--;
+		while (i < j)
+		{
+			temp = lastWord[j];
+			lastWord[j] = lastWord[i];
+			lastWord[i] = temp;
+			i++;
+			j--;
+		}
+	}
+	return lastWord;
 }
